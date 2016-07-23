4401ee48abce9e41785c78dca4f9e2a3c4a35812
Archit Naraniwal <archit.ar23@gmail.com>
2016-07-23 15:51:22 +0530

    Delete trunc.r

--- trunc.r
+++
@@ -1,9 +0,0 @@
-#' trunc_seconds
-#' 
-#' Returns a function that truncates POSIXct times to start of nearest interval
-#'
-#' @param	n 		interval length, in seconds
-#' @export
-trunc_seconds <- function(n) {
-	function(x) as.POSIXct(x - as.numeric(x) %% n)
-}
